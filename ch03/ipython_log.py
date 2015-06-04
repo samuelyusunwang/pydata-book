f6c719fd6670a919b3a1087859c313baae35c65c
samuelyusunwang <samuelyusunwang@gmail.com>
2015-06-04 15:20:27 -0500

    Pydata Ch03
    Test Files

---
+++ ch03/ipython_log.py
@@ -0,0 +1,55 @@
+# IPython log file
+
+get_ipython().magic('cd U:\\My Documents\\GitHub\\pydata-book\\ch03')
+get_ipython().magic('run ipython_bug.py')
+print(ipython_bug.py)
+get_ipython().magic('whos ')
+inspect.getsourcelines(ipython_bug.py)
+a = np.random.randn(100,100)
+get_ipython().magic('timeit np.dot(a, a)')
+a = 1
+get_ipython().magic('pinfo %reset')
+get_ipython().magic('quickref')
+get_ipython().magic('pinfo _')
+_
+a
+_
+__
+___
+get_ipython().magic('magic')
+get_ipython().magic('debug')
+get_ipython().magic('hist')
+get_ipython().magic('pdb')
+get_ipython().magic('pdb')
+get_ipython().magic('paste')
+get_ipython().magic('cpaste')
+get_ipython().magic('page a')
+get_ipython().magic('pinfo %prun')
+get_ipython().magic('who_ls ')
+get_ipython().magic('whos ')
+who
+get_ipython().magic('who')
+get_ipython().magic('whos')
+get_ipython().magic('xdel a')
+get_ipython().magic('whos ')
+who
+get_ipython().magic('who')
+img = plt.imread('stinkbug.png')
+imshow(img)
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+get_ipython().magic('ls ')
+2 ** 27
+_
+_i40
+foo = 'bar'
+foo
+_i27
+_i44
+exec _i44
+exec _i43
+exec(_i43)
+exec(_i44)
+get_ipython().magic('logstart')
+get_ipython().magic('logsoff')
+get_ipython().magic('logoff')
